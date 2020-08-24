fb6eed0aa4bb144577a053b8985a58cbf70f9586
Đào Thanh Tùng <tung.dao@techx.vn>
2020-08-24 17:39:36 +0700

    func Do a job

--- retry.go
+++ retry.go
@@ -2,99 +2,106 @@ package retry
 
 import (
 	"errors"
+	"fmt"
 	"math"
 	"math/rand"
+	"os"
+	"sync"
 	"time"
 )
 
 type Retrier struct {
-	cfg     *Config
-	storage Storage
+	jobFunc      func(jsonArgs string) error
+	cfg          *Config
+	storage      Storage
+	hostname     string
+	stopJobChans map[JobId]chan bool
+	mutex        *sync.Mutex
 }
 
-// New creates a new job in storage,
-// job status normally is running and will be doing by the machine created it
-func (r Retrier) New(jobFunc func() error, jobId JobId, status JobStatus) (
-	*Job, error) {
-	job := &Job{
-		Id:            jobId,
-		Status:        status,
-		NAttempts:     0,
-		LastAttempted: time.Unix(0, 0),
-		NextDelay:     r.cfg.DelayType(0, r.cfg),
-		Errors:        make([]error, 0),
+func NewRetrier(jobFunc func(jsonArgs string) error,
+	cfg *Config, storage Storage) *Retrier {
+	if cfg == nil {
+		cfg = NewDefaultConfig()
 	}
-	return job, nil
+	if storage == nil {
+		storage = NewMemoryStorage()
+	}
+	hostname, _ := os.Hostname()
+	if hostname == "" {
+		hostname = "hostname0"
+	}
+	return &Retrier{
+		jobFunc: jobFunc,
+		cfg:     cfg, storage: storage, hostname: hostname,
+		stopJobChans: make(map[JobId]chan bool), mutex: &sync.Mutex{},
+	}
 }
 
-// do runs a job (newly created or taken from queue jobs in storage)
-func (r Retrier) Do(jobId JobId) {
-	var n uint
-
-	//default
-	config := &Config{
-		attempts:      DefaultAttempts,
-		delay:         DefaultDelay,
-		maxJitter:     DefaultMaxJitter,
-		onRetry:       DefaultOnRetry,
-		retryIf:       DefaultRetryIf,
-		delayType:     DefaultDelayType,
-		lastErrorOnly: DefaultLastErrorOnly,
+// do runs a job (newly created or loaded from queue jobs in storage),
+// :params jobFuncJsonArgs: will be ignored if the jobId existed in storage
+func (r *Retrier) Do(jobId JobId, jobFUncJsonArgsIfCreate string) (*Job, error) {
+	job, err := r.storage.TakeJob(jobId, jobFUncJsonArgsIfCreate)
+	if err != nil {
+		return nil, fmt.Errorf("storage TakeJob: %v", err)
 	}
-
-	//apply opts
-	for _, opt := range opts {
-		opt(config)
-	}
-
-	var errorLog Error
-	if !config.lastErrorOnly {
-		errorLog = make(Error, config.attempts)
-	} else {
-		errorLog = make(Error, 1)
-	}
-
-	lastErrIndex := n
-	for n < config.attempts {
-		err := retryableFunc()
-
-		if err != nil {
-			errorLog[lastErrIndex] = unpackUnrecoverable(err)
-
-			if !config.retryIf(err) {
-				break
-			}
-
-			config.onRetry(n, err)
-
-			// if this is last attempt - don't wait
-			if n == config.attempts-1 {
-				break
-			}
-
-			delayTime := config.delayType(n, config)
-			if config.maxDelay > 0 && delayTime > config.maxDelay {
-				delayTime = config.maxDelay
-			}
-			time.Sleep(delayTime)
+	stopJobChan := make(chan bool)
+	r.mutex.Lock()
+	r.stopJobChans[job.Id] = stopJobChan
+	r.mutex.Unlock()
+	for {
+		err := r.jobFunc(job.JobFuncJsonArgs)
+		job.NAttempts++
+		job.LastAttempted = time.Now()
+		job.LastAttemptedHost = r.hostname
+		job.NextDelay = r.cfg.DelayType(job.NAttempts, r.cfg)
+		if r.cfg.MaxDelay > 0 && job.NextDelay > r.cfg.MaxDelay {
+			job.NextDelay = r.cfg.MaxDelay
+		}
+		if err == nil {
+			job.Errors = append(job.Errors, "")
+			job.Status = Stopped // successfully do the job
 		} else {
-			return nil
+			job.Errors = append(job.Errors, err.Error())
+			if job.NAttempts == r.cfg.MaxAttempts {
+				job.Status = Stopped //  give up after max attempts
+			} else {
+				job.Status = Running
+			}
 		}
-
-		n++
-		if !config.lastErrorOnly {
-			lastErrIndex = n
+		err = r.storage.UpdateJob(job)
+		if err != nil {
+			return job, fmt.Errorf("storage UpdateJob: %v", err)
+		}
+		if job.Status == Stopped {
+			return job, nil
+		}
+		select {
+		case <-time.After(job.NextDelay):
+			continue
+		case <-stopJobChan:
+			job.Status = Stopped
+			err = r.storage.UpdateJob(job)
+			if err != nil {
+				return job, fmt.Errorf("storage UpdateJob: %v", err)
+			}
+			return job, nil
 		}
 	}
-
-	if config.lastErrorOnly {
-		return errorLog[lastErrIndex]
-	}
-	return errorLog
 }
 
-func (r Retrier) Stop(jobId JobId) error {
-	return errors.New("TODO")
+func (r *Retrier) Stop(jobId JobId) error {
+	r.mutex.Lock()
+	stopJobChan, found := r.stopJobChans[jobId]
+	r.mutex.Unlock()
+	if !found {
+		return ErrJobIsNotRunning
+	}
+	stopJobChan <- true
+	r.mutex.Lock()
+	delete(r.stopJobChans, jobId)
+	r.mutex.Unlock()
+	return nil
 }
 
 type Config struct {
@@ -118,32 +125,38 @@ func NewDefaultConfig() *Config {
 
 type DelayTypeFunc func(nAttempts int, config *Config) time.Duration
 
+// a Fibonacci delay sequence,
+// example if base delay is 50ms then 10th retry delay is 3800 ms
 func ExpBackOffJitterDelay(nAttempts int, config *Config) time.Duration {
-	// with a base delay 50ms, delay on 10th retry is 3800 ms
-	delay := config.Delay * time.Duration(math.Pow(1.618, float64(nAttempts)))
+	delay := config.Delay * time.Duration(math.Pow(1.618, float64(nAttempts-1)))
 	delay += time.Duration(rand.Int63n(int64(config.MaxJitter)))
 	return delay
 }
 
 type Job struct {
-	Id            JobId
-	Status        JobStatus
-	NAttempts     int // number of made attempts
-	LastAttempted time.Time
-	NextDelay     time.Duration
-	Errors        []error // errors of attempts
+	JobFuncJsonArgs   string
+	Id                JobId // unique
+	Status            JobStatus
+	NAttempts         int // number of tried attempts
+	LastAttempted     time.Time
+	LastAttemptedHost string // human readable
+	NextDelay         time.Duration
+	Errors            []string // errors of attempts
 }
 
 func (j Job) LastErr() error {
 	if len(j.Errors) < 1 {
 		return nil
 	}
-	return j.Errors[len(j.Errors)-1]
+	lastErrMsg := j.Errors[len(j.Errors)-1]
+	if lastErrMsg == "" {
+		return nil
+	}
+	return errors.New(lastErrMsg)
 }
 
-// JobId must be unique, example catted from a meaningful prefix and a UUID
-type JobId string
-type JobStatus string
+type JobId string     // unique, example catted from a meaningful prefix and a UUID
+type JobStatus string // enum
 
 // JobStatus enum
 const (
@@ -153,21 +166,43 @@ const (
 )
 
 type Storage interface {
-	// change some queue jobs to running
-	// (understand as the jobs are doing on the machineId)
-	TakeJobs(machineId string, maxJobs int) []*Job
-	// Check all running jobs, if a job retried too long time ago change it
-	// to a queue job
-	RequeueCrashJobs() []*Job
+	// read jobId from queue jobs, create if not existed,
+	// return err if the job is running (maybe on other machine),
+	// update the jobs status to running
+	TakeJob(jobId JobId, jsonArgsIfCreate string) (*Job, error)
+	// take some queuing jobs to run,
+	// update the jobs status to running
+	TakeJobs(maxJobs int) ([]*Job, error)
+	UpdateJob(*Job) error // call after one attempt
+	StopJob(jobId JobId) (*Job, error)
+
+	// Check all running jobs, if a job lastAttempted too long time ago change it
+	// status to queue
+	RequeueHangingJobs() ([]*Job, error)
 }
 
 type MemoryStorage struct {
 }
 
-func (s *MemoryStorage) TakeJobs(machineId string, maxJobs int) []*Job {
-	return nil // TODO MemoryStorage TakeJobs
+func NewMemoryStorage() *MemoryStorage { return &MemoryStorage{} }
+func (s *MemoryStorage) TakeJob(jobId JobId, jsonArgsIfCreate string) (*Job, error) {
+	return nil, errNotImplemented // TODO MemoryStorage ReadJob
+}
+func (s *MemoryStorage) TakeJobs(maxJobs int) ([]*Job, error) {
+	return nil, errNotImplemented // TODO MemoryStorage TakeJobs
+}
+func (s *MemoryStorage) UpdateJob(*Job) error {
+	return errNotImplemented // TODO MemoryStorage UpdateJob
+}
+func (s *MemoryStorage) StopJob(jobId JobId) (*Job, error) {
+	return nil, errNotImplemented
+}
+func (s *MemoryStorage) RequeueHangingJobs() ([]*Job, error) {
+	return nil, errNotImplemented // TODO MemoryStorage RequeueCrashJobs
 }
 
-func (s *MemoryStorage) RequeueCrashJobs() []*Job {
-	return nil // TODO MemoryStorage RequeueCrashJobs
-}
+var (
+	ErrJobRunning      = errors.New("job is running")
+	ErrJobIsNotRunning = errors.New("job is not running on this machine")
+	errNotImplemented  = errors.New("not implemented")
+)
